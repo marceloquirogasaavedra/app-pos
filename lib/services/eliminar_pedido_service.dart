d3c7dcfe7f4dc91310fac12674612f3cfd990be4
marceloquirogasaavedra <120491745+marceloquirogasaavedra@users.noreply.github.com>
2024-11-29 14:08:00 -0400

    Update eliminar_pedido_service.dart

--- lib/services/eliminar_pedido_service.dart
+++ lib/services/eliminar_pedido_service.dart
@@ -1,7 +1,7 @@
 import 'package:http/http.dart' as http;
 
 class EliminarPedidoService {
-  final String baseUrl = "http://localhost:8080/pos/pos/pedido";
+  final String baseUrl = "http://98.85.18.243:8080/pos/pos/pedido";
 
   Future<void> eliminarPedido(int idPedido, String token) async {
     final response = await http.delete(
